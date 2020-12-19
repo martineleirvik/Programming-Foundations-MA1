dcf308dbc61008f2cb553748657923cbdf55bbdd
martineleirvik <leirvikmartine@gmail.com>
2020-12-19 23:35:01 +0100

    First

---
+++ js/script1.js
@@ -0,0 +1,54 @@
+console.log("This is script 1");
+
+//question 1
+var integer = 6;
+var decimal = 10.4;
+
+//question 2
+var daysOfTheWeek = "Monday" + "-" + "Tuesday" + "-" + "Wednesday" + "-" + "Thursday" + "-" + "Friday";
+
+//question 3
+var addition = 2 + 5;
+var subtraction = 9 - 3;
+var multiplication = 2 * 2;
+var division = 15 / 3;
+
+//question 4
+var signIn = false;
+
+//question 5
+var sentence = "The frogs are angry.";
+
+//question 6
+var timeElapsed;
+var totalPrice;
+
+//question 7
+var number1 = "53";
+var number2 = "44.6";
+
+var convertedNumber1 = parseInt(number1);
+var convertedNumber2 = parseFloat(number2);
+
+//question 8
+var pet = "donkey";
+var age = 89;
+var discountApplied = true;
+var timeLeft;
+
+var typeOfPet = typeof pet;
+console.log(typeOfPet);
+
+var typeOfAge = typeof age;
+console.log(typeOfAge);
+
+var typeOfDiscountApplied = typeof discountApplied;
+console.log(typeOfDiscountApplied);
+
+var typeOfTimeLeft = typeof timeLeft;
+console.log/typeOfTimeLeft;
+
+//question 9
+
+
+//question 10
