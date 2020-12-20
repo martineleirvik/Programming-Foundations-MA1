efa00101ceeac6478b50936a5679ee69cae82d1d
martineleirvik <leirvikmartine@gmail.com>
2020-12-20 15:17:51 +0100

    done

--- js/script1.js
+++ js/script1.js
@@ -1,54 +1,46 @@
 console.log("This is script 1");
 
-//question 1
-var integer = 6;
-var decimal = 10.4;
 
-//question 2
-var daysOfTheWeek = "Monday" + "-" + "Tuesday" + "-" + "Wednesday" + "-" + "Thursday" + "-" + "Friday";
+// question 1
 
-//question 3
-var addition = 2 + 5;
-var subtraction = 9 - 3;
-var multiplication = 2 * 2;
-var division = 15 / 3;
+var name = "John" ;
 
-//question 4
-var signIn = false;
+var price = 5 
+
+var loggedIn = false
+
+
+// question 2
+
+var firstName = "Martine" ;
+var lastName = "Leirvik" ;
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+
+// question 3
+
+var animal = "frog"
+var typeOfAnimal = typeof(animal);
+console.log(typeOfAnimal);
+//string
+
+
+// question 4
+
+var orderHasShipped = true;
+
+if(orderHasShipped === true) {
+    console.log(orderHasShipped);
+} else {
+    console.log("The order did not ship")
+}
+
 
 //question 5
-var sentence = "The frogs are angry.";
 
-//question 6
-var timeElapsed;
-var totalPrice;
-
-//question 7
-var number1 = "53";
-var number2 = "44.6";
-
-var convertedNumber1 = parseInt(number1);
-var convertedNumber2 = parseFloat(number2);
-
-//question 8
-var pet = "donkey";
-var age = 89;
-var discountApplied = true;
-var timeLeft;
-
-var typeOfPet = typeof pet;
-console.log(typeOfPet);
-
-var typeOfAge = typeof age;
-console.log(typeOfAge);
-
-var typeOfDiscountApplied = typeof discountApplied;
-console.log(typeOfDiscountApplied);
-
-var typeOfTimeLeft = typeof timeLeft;
-console.log/typeOfTimeLeft;
-
-//question 9
-
-
-//question 10
+for (var i = 7; i <= 13; i++) {
+    console.log(i);
+}
--- js/script2.js
+++ js/script2.js
@@ -1,1 +1,3 @@
-console.log("This is script 2")
+console.log("This is script 2");
+
+
