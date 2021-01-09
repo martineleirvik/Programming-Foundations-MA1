c013fa01de763094611e6da5b192191a7b16cb92
martineleirvik <leirvikmartine@gmail.com>
2021-01-09 16:24:33 +0100

    little

--- js/script2.js
+++ js/script2.js
@@ -1,3 +1,32 @@
 console.log("This is script 2");
 
+// question 1
+
+var pets = [
+	{
+		type: "cat",
+		age: 5.5,
+	},
+	{
+		type: "dog",
+		age: 3.8,
+	},
+	{
+		type: "parrot",
+		age: 4.0,
+	},
+];
+
+console.log(pets):
+
+
+//question 2
+
+function logCode() {
+    console.log("one");
+}
+
+logCode(); 
+
+
 
